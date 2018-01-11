ead16249f31896e9b4ffd956a573ffdb2b79f525
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2018-01-11 18:17:42 +0800

    Add files via upload

---
+++ DB/Controllers/DeleteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB.Controllers
+{
+    public class DeleteController : Controller
+    {
+        // GET: Delete
+        public ActionResult Index(Model.Data Data)
+        {
+            Service.SQL_Delete SD = new Service.SQL_Delete();
+            SD.Delete(Data.OrderId);            
+            if (Data.EmpName == null)
+            {
+                Data.EmpName = "null";
+            }
+            if (Data.CpyName == null)
+            {
+                Data.CpyName = "null";
+            }
+            return RedirectToAction("Index", "Inquire",
+                new {
+                    OrderId = Data.OrderId,
+                    CustName = Data.CustName,
+                    EmpName = Data.EmpName,
+                    CpyName = Data.CpyName,
+                    OrderDate = Data.OrderDate,
+                    RequiredDate = Data.RequiredDate,
+                    ShippedDate = Data.ShippedDate        
+                });
+        }
+    }
+}
